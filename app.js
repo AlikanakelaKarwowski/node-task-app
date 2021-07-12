86ac0c66563c6f51f479d05656feda207e20b6e2
A.Karwowski <akk42@zips.uakron.edu>
2021-07-12 15:25:47 -0400

    insert one

--- app.js
+++ app.js
@@ -6,10 +6,22 @@ const MongoClient = mongodb.MongoClient
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
     if (error) {
         return console.log('Unable to connect to database!')
     }
 
-    console.log('Connected Correctly')
+    const db = client.db(databaseName)
+    db.collection('users').insertOne(
+        {
+            name: 'Alex',
+            age: 24,
+        },
+        (error, result) => {
+            if (error) {
+                return console.log('Unable to insert user')
+            }
+            console.log(result.ops)
+        }
+    )
 })
