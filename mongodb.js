419efdb66960f5aa4d547dc25cd1c10c162c492f
A.Karwowski <akk42@zips.uakron.edu>
2021-07-12 15:47:28 -0400

    learn about GUID

--- mongodb.js
+++ mongodb.js
@@ -1,33 +1,39 @@
 // CRUD Create Read Update Delete
 
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+// const mongodb = require('mongodb')
+// const MongoClient = mongodb.MongoClient
+// const ObjectID = mongodb.ObjectID
+
+const { MongoClient, ObjectID } = require('mongodb')
+
+const id = new ObjectID()
+console.log(id)
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
-    if (error) {
-        return console.log('Unable to connect to database!')
-    }
+// MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+//     if (error) {
+//         return console.log('Unable to connect to database!')
+//     }
 
-    const db = client.db(databaseName)
-    db.collection('users').insertMany(
-        [
-            {
-                name: 'Alex',
-                age: 24,
-            },
-            {
-                name: 'Loki',
-                age: 1.5,
-            },
-        ],
-        (error, result) => {
-            if (error) {
-                return console.log('Unable to insert users')
-            }
-            console.log(result.ops)
-        }
-    )
-})
+//     const db = client.db(databaseName)
+//     db.collection('users').insertMany(
+//         [
+//             {
+//                 name: 'Alex',
+//                 age: 24,
+//             },
+//             {
+//                 name: 'Loki',
+//                 age: 1.5,
+//             },
+//         ],
+//         (error, result) => {
+//             if (error) {
+//                 return console.log('Unable to insert users')
+//             }
+//             console.log(result.ops)
+//         }
+//      )
+// })
