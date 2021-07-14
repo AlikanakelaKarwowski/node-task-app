e1bcfc369cdbf643600f2275ee1f392d0055c85a
A.Karwowski <akk42@zips.uakron.edu>
2021-07-14 16:05:52 -0400

    mongoose and validator for models.

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
@@ -9,9 +10,27 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 const User = mongoose.model('User', {
     name: {
         type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        },
     },
     age: {
         type: Number,
+        required: true,
+        validate(value) {
+            if (value < 18) {
+                throw new Error('You must be 18 y/o or older')
+            } else if (value > 120) {
+                throw new Error('You are too old')
+            }
+        },
     },
 })
 
@@ -24,29 +43,30 @@ const Task = mongoose.model('Task', {
     },
 })
 
-// const me = new User({
-//     name: 'Andrew',
-//     age: 'twenty seven',
-// })
-
-// me.save()
-//     .then(() => {
-//         console.log(me)
-//     })
-//     .catch((err) => {
-//         console.log('Error!', err)
-//     })
-
-const groomer = new Task({
-    description: 'Call the dog groomer',
-    completed: false,
+const me = new User({
+    name: 'Lamedrew',
+    age: 18,
+    email: 'lame@drew.com',
 })
 
-groomer
-    .save()
+me.save()
     .then(() => {
-        console.log(groomer)
+        console.log(me)
     })
     .catch((err) => {
         console.log('Error!', err)
     })
+
+// const groomer = new Task({
+//     description: 'Call the dog groomer',
+//     completed: false,
+// })
+
+// groomer
+//     .save()
+//     .then(() => {
+//         console.log(groomer)
+//     })
+//     .catch((err) => {
+//         console.log('Error!', err)
+//     })
