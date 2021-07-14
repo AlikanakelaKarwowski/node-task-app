81f95e27c0ec9d9b9f75045548c47f0c624da095
A.Karwowski <akk42@zips.uakron.edu>
2021-07-14 19:53:57 -0400

    move task to own file. dont load entire mongoose module

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,19 +1,7 @@
-const mongoose = require('mongoose')
+const { connect } = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
-})
-
-const Task = mongoose.model('Task', {
-    description: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    completed: {
-        type: Boolean,
-        default: false,
-    },
 })
