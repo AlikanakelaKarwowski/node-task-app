1dccfeff47644e82755ffd6e2b2ff8211af16e1f
A.Karwowski <akk42@zips.uakron.edu>
2021-07-14 17:17:26 -0400

    clean up models to add requirements and sanitization

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
+const { isEmail } = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
@@ -11,62 +11,74 @@ const User = mongoose.model('User', {
     name: {
         type: String,
         required: true,
+        trim: true,
     },
     email: {
         type: String,
         required: true,
+        trim: true,
+        lowercase: true,
         validate(value) {
-            if (!validator.isEmail(value)) {
+            if (!isEmail(value)) {
                 throw new Error('Email is invalid')
+            }
+        },
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 7,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Your password cant contain the word password')
             }
         },
     },
     age: {
         type: Number,
         required: true,
-        validate(value) {
-            if (value < 18) {
-                throw new Error('You must be 18 y/o or older')
-            } else if (value > 120) {
-                throw new Error('You are too old')
-            }
-        },
+        min: [18, 'Must be 18 y/o or older'],
+        max: [120, 'You are too old'],
     },
 })
 
 const Task = mongoose.model('Task', {
     description: {
         type: String,
+        required: true,
+        trim: true,
     },
     completed: {
         type: Boolean,
+        default: false,
     },
 })
 
-const me = new User({
-    name: 'Lamedrew',
-    age: 18,
-    email: 'lame@drew.com',
-})
-
-me.save()
-    .then(() => {
-        console.log(me)
-    })
-    .catch((err) => {
-        console.log('Error!', err)
-    })
-
-// const groomer = new Task({
-//     description: 'Call the dog groomer',
-//     completed: false,
+// const me = new User({
+//     name: '   Alexander   ',
+//     email: '  MYEmail@mlm.COM',
+//     password: '  passwor',
+//     age: 18,
 // })
 
-// groomer
-//     .save()
+// me.save()
 //     .then(() => {
-//         console.log(groomer)
+//         console.log(me)
 //     })
 //     .catch((err) => {
 //         console.log('Error!', err)
 //     })
+
+const groomer = new Task({
+    description: '    Call the dog groomer    ',
+})
+
+groomer
+    .save()
+    .then(() => {
+        console.log(groomer)
+    })
+    .catch((err) => {
+        console.log('Error!', err)
+    })
