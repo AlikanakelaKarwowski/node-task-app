db9b0e458576d661573395aa30d47b8cf07a5c93
A.Karwowski <akk42@zips.uakron.edu>
2021-07-19 12:30:10 -0400

    change task routes to async await

--- src/index.js
+++ src/index.js
@@ -41,42 +41,36 @@ app.get('/users/:id', async (req, res) => {
     }
 })
 
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
-
-    task.save()
-        .then(() => {
-            res.status(201).send(task)
-        })
-        .catch((err) => {
-            res.status(400).send(err)
-        })
+    try {
+        await task.save()
+        res.status(201).send(task)
+    } catch (err) {
+        res.status(400).send(err)
+    }
 })
 
-app.get('/tasks', (req, res) => {
-    Task.find({})
-        .then((tasks) => {
-            res.send(tasks)
-        })
-        .catch((err) => {
-            res.status(400).send()
-        })
+app.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find({})
+        res.send(tasks)
+    } catch (err) {
+        res.status(500).send(err)
+    }
 })
 
-app.get('/tasks/:id', (req, res) => {
+app.get('/tasks/:id', async (req, res) => {
     const _id = req.params.id
-
-    Task.findById(_id)
-        .then((task) => {
-            if (!task) {
-                return res.status(404).send()
-            }
-
-            res.send(task)
-        })
-        .catch((err) => {
-            res.status(500).send(err)
-        })
+    try {
+        const task = await Task.findById(_id)
+        if (!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    } catch (err) {
+        res.status(500).send(err)
+    }
 })
 
 app.listen(port, () => {
