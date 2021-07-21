47c8c1a7b16b4c08c4044a0d604a711356af7816
A.Karwowski <akk42@zips.uakron.edu>
2021-07-21 12:14:49 -0400

    add delete endpoint for tasks

--- src/index.js
+++ src/index.js
@@ -130,6 +130,20 @@ app.patch('/tasks/:id', async (req, res) => {
     }
 })
 
+app.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    } catch (err) {
+        res.status(500).send(err)
+    }
+})
+
 app.listen(port, () => {
     console.log(`Server is up on port: ${port}`)
 })
