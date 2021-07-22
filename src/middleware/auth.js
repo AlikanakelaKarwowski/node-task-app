32a28d4d8dcf34e4169138868970e35b4b0ad688
A.Karwowski <akk42@zips.uakron.edu>
2021-07-22 13:30:37 -0400

    auth token middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,6 +1,21 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+
 const auth = async (req, res, next) => {
-    console.log('auth middleware')
-    next()
+    try {
+        const token = req.header('Authorization').replace('Bearer ', '')
+        const decoded = jwt.verify(token, 'this is a test function')
+        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
+
+        if (!user) {
+            throw new Error()
+        }
+
+        req.user = user
+        next()
+    } catch (err) {
+        res.status(401).send({ error: 'Please Authenticate' })
+    }
 }
 
 module.exports = auth
