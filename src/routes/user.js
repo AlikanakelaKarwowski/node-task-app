43f89583117ba5538094c9b80fbb1e66a93a5d64
A.Karwowski <akk42@zips.uakron.edu>
2021-07-22 13:37:01 -0400

    add logout endpoint

--- src/routes/user.js
+++ src/routes/user.js
@@ -25,6 +25,18 @@ router.post('/users/login', async (req, res) => {
     }
 })
 
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save()
+
+        res.send()
+    } catch (err) {
+        res.status(500).send(err)
+    }
+})
 router.get('/users/me', auth, async (req, res) => {
     res.send(req.user)
 })
