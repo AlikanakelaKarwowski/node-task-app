5beb3bf19cf2f4f4f1f29446ac99f703155f4070
A.Karwowski <akk42@zips.uakron.edu>
2021-07-22 10:28:43 -0400

    create find by credentials function

--- src/models/user.js
+++ src/models/user.js
@@ -10,6 +10,7 @@ const userSchema = new Schema({
     },
     email: {
         type: String,
+        unique: true,
         required: true,
         trim: true,
         lowercase: true,
@@ -38,12 +39,25 @@ const userSchema = new Schema({
     },
 })
 
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email })
+    if (!user) {
+        throw new Error('Unable to Login')
+    }
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
+        throw new Error('Unable to Login')
+    }
+
+    return user
+}
+
 userSchema.pre('save', async function (next) {
     const user = this
     if (user.isModified('password')) {
         user.password = await bcrypt.hash(user.password, 8)
     }
-    console.log('just before saving')
     next()
 })
 
